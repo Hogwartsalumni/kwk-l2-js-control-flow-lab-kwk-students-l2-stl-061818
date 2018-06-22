3e674e18c48710bc885c30fad34df3a9cde51380
Scarlett Patton <scarwars05@gmail.com>
2018-06-22 14:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function scuberGreetingForFeet(number) {
 }
 
 function ternaryCheckCity(place) {
-  return (place = 'NYC' ? 'Ok, sounds good.' : 'No go.');
+  return (city = 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
 function switchOnCharmFromTip(response) {
