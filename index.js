335e57768e4aa0cf5b8c13c82879b2f5b85df721
Scarlett Patton <scarwars05@gmail.com>
2018-06-22 14:38:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number) {
+  if (number <= 400) {
+    return 'This one is on me!';
+  } else if (number > 2000 && number <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (number > 2500) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(place) {
+  return (place === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+}
+
+function switchOnCharmFromTip(response) {
+  switch (response) {
+    case 'generous':
+       return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default :
+      return 'Bye.';
+  }
+}
